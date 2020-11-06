7d9e97b407e0470ffec62c47bf0eb67a3fff8fc6
Vitya <exam@mail.ru>
2020-11-06 23:28:23 +0300

    part 4

--- js/script.js
+++ js/script.js
@@ -1,58 +1,17 @@
 "use strict ";
 
-function start(){
-    numberOfFilms = +prompt("Сколько фильмов посмотрели?", "");
 
-    while (isNaN(numberOfFilms) || numberOfFilms == "") {
-        numberOfFilms = +prompt("Сколько фильмов посмотрели?", "");
-    }
-}
 
-function rememberMyFilms(){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Какой крайний фильм смотрели?", ""),
-              b = +prompt("Оценка", "");
-    
-        if(a == null || b == null || a == "" || a.length > 50){
-            i--;
-            continue;
-        }
-    
-        personalMovieDB.movies[a] = b;
-    }
-    
-}
-function detectPersonalLevel(){
-    if (personalMovieDB.count < 10){
-        console.log("Просмотренно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count > 30){
-        console.log("Вы киноман");
-    } else {
-        console.log("Ошибка");
-    }
-}
 
-function showMyDB() {
-    if (personalMovieDB.privat == false){
-        console.log(personalMovieDB);
-    }
-}
 
-function writeYourGenres(){
-    for(let i = 1; i < 4; i++){
-        const question = prompt(`Ваш любимый жанр под номером ${i}`);
-        // personalMovieDB.genres[i] = question;
-        personalMovieDB.genres.push(question);
-    }
-    console.log(`Ваш массив жанров: ${personalMovieDB.genres}`);
-}
+
+
+
 
 
 
 let numberOfFilms;
-start();
+
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -60,10 +19,73 @@ let personalMovieDB = {
     actors: {},
     genres: [],
     privat: false,
+    writeYourGenres : function (){
+        for(let i = 1; i < 4; i++){
+            const question = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (question == "" || question == null){
+                i--;
+                continue;
+            }
+            personalMovieDB.genres.push(question);
+        }
+        personalMovieDB.genres.forEach(function(elem, index, arr) {
+            console.log(`Любимый жанр №${index + 1} - это ${arr[index]}`);
+        });
+    },
+    showMyDB: function () {
+        if (personalMovieDB.privat == false){
+            console.log(personalMovieDB);
+        }
+    },
+    detectPersonalLevel : function (){
+        if (personalMovieDB.count < 10){
+            console.log("Просмотренно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count > 30){
+            console.log("Вы киноман");
+        } else {
+            console.log("Ошибка");
+        }
+    },
+    rememberMyFilms: function (){
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Какой крайний фильм смотрели?", ""),
+                  b = +prompt("Оценка", "");
+        
+            if(a == null || b == null || a == "" || a.length > 50){
+                i--;
+                continue;
+            }
+        
+            personalMovieDB.movies[a] = b;
+        }
+        
+    },
+    start : function (){
+        numberOfFilms = +prompt("Сколько фильмов посмотрели?", "");
+    
+        while (isNaN(numberOfFilms) || numberOfFilms == "") {
+            numberOfFilms = +prompt("Сколько фильмов посмотрели?", "");
+        }
+        personalMovieDB.count = numberOfFilms;
+    },
+    toggleVisibleMyDB : function() {
+        if (!personalMovieDB.privat){
+            personalMovieDB.privat = true;
+        } else {
+            personalMovieDB.privat = false;
+        }
+    }
+
+
 };
 
-rememberMyFilms();
-detectPersonalLevel();
-showMyDB();
+let {start, rememberMyFilms,detectPersonalLevel, showMyDB, toggleVisibleMyDB, writeYourGenres} = personalMovieDB;
+// start();
+// rememberMyFilms();
+// detectPersonalLevel();
+// toggleVisibleMyDB();
+// showMyDB();
 writeYourGenres();
 alert(JSON.stringify(personalMovieDB, null, 3 ));
