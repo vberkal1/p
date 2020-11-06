0cbbfdad053e6d3dd039ca8e1d10924d81601220
Vitya <exam@mail.ru>
2020-11-06 13:38:53 +0300

    lesson 15 part 2

--- js/script.js
+++ js/script.js
@@ -10,9 +10,27 @@ let personalMovieDB = {
     privat: false,
 };
 
-const a = prompt("Какой крайний фильм смотрели?", ""),
-      b = +prompt("Оценка", "");
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Какой крайний фильм смотрели?", ""),
+          b = +prompt("Оценка", "");
 
-personalMovieDB.movies[a] = b;
+    if(a == null || b == null || a == "" || a.length > 50){
+        i--;
+        continue;
+    }
+
+    personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10){
+    console.log("Просмотренно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30){
+    console.log("Вы киноман");
+} else {
+    console.log("Ошибка");
+}
+
 
 alert(JSON.stringify(personalMovieDB, null, 3 ));
